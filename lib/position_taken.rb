dfba8d14136522cba6b15c4b97112b4cdccfbcc3
mcglaser <michaelcharlesglaser@gmail.com>
2016-02-08 11:00:21 +0000

    nitrous-backup at Mon Feb  8 11:00:21 UTC 2016

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,14 @@
+def valid_move?(board, position)
+  position = position.to_i
+  if position_taken? == false && board[position - 1] <= 8
+     true
+  else
+     false
+  end
+end
+
+
+
 # code your #position_taken? method here!
 def position_taken?(board, position)
   if board[position] == " " || board[position] == "" || board[position] == nil
@@ -6,3 +17,5 @@ def position_taken?(board, position)
      true
   end
 end
+
+
